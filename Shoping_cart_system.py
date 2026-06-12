ef1f855bcdee70444462f36e134574c3d8637a4a
Muhammed Rishan T <rishan000rishu@gmail.com>
2026-06-12 13:39:33 +0400

    Added file handling method to Shoping cart system.

--- Shoping_cart_system.py
+++ Shoping_cart_system.py
@@ -1,4 +1,37 @@
-cart = []
+import os
+
+cart = {}
+
+file_name = 'Cart.txt'
+
+def save_data():
+
+    with open(file_name, 'w')as file:
+
+        for name, price in cart.items():
+
+            file.write(f'Name: {name},Price: {price}\n')
+
+def load_data():
+
+    if not os.path.exists(file_name):
+        return
+    
+    try:
+        with open(file_name, 'r')as file:
+
+            for line in file:
+
+                parts = line.strip().split(',')
+
+                name = parts[0].split(':')[1].strip()
+                price = parts[1].split(':')[1].strip()
+
+                cart[name] = int(price)
+
+    except FileNotFoundError:
+        pass
+
 
 while True:
     print('''
@@ -12,44 +45,47 @@ while True:
     choice = input('Enter tour choice: ')
 
     if choice =='1':
-        item_name = input('Enter item name to add: ')
-        item_price = float(input('Enter item price to add: '))
+        load_data()
+        name = input('Enter item name to add: ')
+        price = int(input('Enter item price to add: '))
 
-        item = {
-            'name':item_name,
-            'price':item_price
-        }
+        cart[name] = price
 
-        cart.append(item)
-        print(f'{item_name} added to cart successfully...')
+        save_data()
+        print(f'{name} added to cart...')
 
     elif choice =='2':
+        load_data()
         if len(cart) ==0:
             print('Cart is empty!!!')
         else:
-            print('\n=====Cart Items=====')
-            for item in cart:
-                print(f'{item['name']} - ₹{item['price']}')
+            print('\n=====Cart Items=====\n')
+            for name, price in cart.items():
+                print(f'{name} - ₹{price}')
+                
 
     elif choice =='3':
-        item_name = input('Enter item name to remove: ')
 
-        found = False
-        for item in cart:
-            if item['name'].lower() == item_name.lower():
-                cart.remove(item)
-                print(f'{item_name} removed successfully...')
-                
-                found = True
-                break
-        if not found:
-            print('Item not in cart!!')
+        load_data()
+        name = input('Enter item name to remove: ')
+
+        if name in cart:
+    
+            del cart[name]
+            save_data()
+
+            print(f'{name} removed successfully...')
+        
+        else:
+            print('Item not in the cart !')
 
     elif choice =='4':
+
+        load_data()
         total = 0
 
-        for item in cart:
-            total += item['price']
+        for name in cart:
+            total += cart[name]
 
         print(f'Total Bill: ₹{total}')
 

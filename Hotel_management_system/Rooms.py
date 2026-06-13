5e8fa77d7ebf57a4199e3a0a4a7f958698655850
Muhammed Rishan T <rishan000rishu@gmail.com>
2026-06-13 23:50:02 +0400

    Just created a menu and some methods and functions.

---
+++ Hotel_management_system/Rooms.py
@@ -0,0 +1,84 @@
+import csv 
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+file_name = os.path.join(BASE_DIR, 'rooms.csv')
+
+rooms = []
+
+def add_rooms():
+
+    load_room_data()
+
+    room_no = int(input('Enter room no: '))
+
+    for room in rooms:
+
+        if room['Room no'] == room_no:
+            
+            print('Room already exists !')
+            return
+
+    room_type = input('Enter room type: (single/double) ')
+    room_price = int(input('Enter room price: '))
+
+    room = {
+        'Room no':room_no,
+        'Room type':room_type,
+        'Room price':room_price
+    }
+
+    rooms.append(room)
+    
+    save_room_data()
+    print('Room data saved..')
+
+
+def save_room_data():
+
+    with open(file_name, 'w', newline='')as file:
+
+        writer = csv.writer(file)
+
+        writer.writerow(['Room no','Room type','Room price'])
+
+        for room in rooms:
+
+            writer.writerow([
+                room['Room no'],
+                room['Room type'],
+                room['Room price']
+            ])
+
+def load_room_data():
+
+    rooms.clear()
+
+    try:
+        with open(file_name, 'r') as file:
+
+            reader = csv.DictReader(file)
+
+            for row in reader:
+
+                rooms.append({
+                    'Room no': int(row['Room no']),
+                    'Room type': row['Room type'],
+                    'Room price': int(row['Room price'])
+                })
+    
+    except FileNotFoundError:
+        pass
+
+
+while True:
+    print('''
+1. Add Rooms
+2. View Rooms
+3. Edit Room Details
+4. Exit''')
+    
+    choice = input('Enter your choice: ')
+
+    if choice =='1':
+        add_rooms()

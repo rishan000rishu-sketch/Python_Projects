28f74d29402b9f243120a2006fe889425cba256c
Muhammed Rishan T <rishan000rishu@gmail.com>
2026-06-16 10:32:05 +0530

    Added Book Room Function And Make Some Modifications

--- Hotel_management_system/Hotel_management_system.py
+++ Hotel_management_system/Hotel_management_system.py
@@ -1,18 +1,63 @@
 from Rooms import rooms, load_room_data, save_room_data
+from datetime import datetime
+import os
+import csv
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+customer_file = os.path.join(BASE_DIR, "customers.csv")
+bill_file = os.path.join(BASE_DIR, "bills.csv")
 
 def display_rooms():
     load_room_data()
+
+    if len(rooms) ==0:
+        print('\nRooms not Found !')
+        return
 
     print('\n======Available Rooms======\n')
 
     for room in rooms:
 
-        print(
-            f'Room {room['Room no']}',
-            f'{room['Room type']}',
-            f'₹{room['Room price']}',
-            f'{room['Room status']}',
-        )
+        print(f'''
+Room No     : {room['Room no']}
+Room Type   : {room['Room type']}
+Room Price  : {room['Room price']}
+Room Status : {room['Room status']}
+''')
+
+def book_room():
+
+    room_no = int(input('Enter Room No: '))
+
+    for room in rooms:
+
+        if room['Room no'] == room_no:
+
+            if room['Room status'] != 'Available':
+                print('\nRoom Not Available !')
+                return
+
+            name = input('Enter Customer Name: ')
+            phone = int(input('Enter Phone No: '))
+            days = int(input('Days Stay: '))
+
+            with open(customer_file, 'a', newline='') as file:
+
+                writer = csv.writer(file)
+
+                if os.path.getsize(customer_file) ==0:
+                    writer.writerow(['Room no','Customer Name','Phone','Days Stay'])
+
+                writer.writerow([room_no,name,phone,days])
+
+                room['Room status'] = 'Booked'
+
+                save_room_data()
+                print('Room Booked Successfully..')
+                return
+            
+    print('Room Not Found !')
 
 while True:
 
@@ -21,15 +66,16 @@ while True:
     print("2. Book Room")
     print("3. Check In")
     print("4. Check Out")
-    print("5. Exit")
+    print("5. Save bill")
+    print("6. Exit")
 
     choice = input("Enter Choice: ")
 
     if choice == "1":
         display_rooms()
 
-    # elif choice == "2":
-    #     book_room()
+    elif choice == "2":
+        book_room()
 
     # elif choice == "3":
     #     check_in()

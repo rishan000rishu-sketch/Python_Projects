3236c82d3a39bc94a76b07bcff14b972aa427e6c
Muhammed Rishan T <rishan000rishu@gmail.com>
2026-06-16 11:53:11 +0530

    Created Check out Function and Bill system in the Program

--- Hotel_management_system/Hotel_management_system.py
+++ Hotel_management_system/Hotel_management_system.py
@@ -82,6 +82,59 @@ def check_in():
 
     print('Room Not Found !')
 
+def check_out():
+
+    load_room_data()
+
+    room_no = int(input('Enter Room No: '))
+
+    for room in rooms:
+
+        if room['Room no'] == room_no:
+
+            if room['Room status'] != 'Occupied':
+
+                print('Room Not Occupied !')
+                return
+            
+            days = int(input('Enter No Days Stayed: '))
+
+            total = room['Room price'] * days
+
+            save_bill(
+                room_no,
+                room['Room price'],
+                days,
+                total
+            )
+
+            room['Room status'] = 'Available'
+            save_room_data()
+
+            print('\nCheck Out Successful...')
+            print('Bill Amount: ',total)
+            return
+        
+    print('Room Not Found !')
+
+def save_bill(room_no, price, days, total):
+
+    bill_id = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+
+    with open(bill_file, 'a', newline='') as file:
+
+        writer = csv.writer(file)
+
+        if os.path.getsize(bill_file) ==0:
+            writer.writerow(['Bill_ID','Room NO','Room Price','Days Stay','Total Amount'])
+
+        writer.writerow([
+            bill_id,
+            room_no,
+            price,
+            days,
+            total
+        ])            
 
 while True:
 
@@ -90,8 +143,7 @@ while True:
     print("2. Book Room")
     print("3. Check In")
     print("4. Check Out")
-    print("5. Save bill")
-    print("6. Exit")
+    print("5. Exit")
 
     choice = input("Enter Choice: ")
 
@@ -104,11 +156,11 @@ while True:
     elif choice == "3":
         check_in()
 
-    # elif choice == "4":
-    #     check_out()
+    elif choice == "4":
+        check_out()
 
     elif choice == "5":
-        print("Thank You!")
+        print("Thank You For Coming 😊")
         break
 
     else:

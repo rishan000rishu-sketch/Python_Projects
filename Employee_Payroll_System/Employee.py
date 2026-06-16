f6006dac3e826812a37ff9c5a4a69d68762810b2
Muhammed Rishan T <rishan000rishu@gmail.com>
2026-06-17 00:07:13 +0530

    Added Employee_Payroll_System Folder.

---
+++ Employee_Payroll_System/Employee.py
@@ -0,0 +1,33 @@
+
+class Employee:
+
+    def __init__(
+        self,
+        emp_id,
+        name,
+        department,
+        destination,
+        basic_salary,
+        allowance,
+        deductions
+    ):
+        
+        self.emp_id = emp_id
+        self.name = name
+        self.department = department
+        self.destination = destination
+        self.basic_salary = basic_salary
+        self.allowance = allowance
+        self.deductions = deductions
+
+    def to_dict(self):
+
+        return{
+            'Emp_ID': self.emp_id,
+            'Name': self.name,
+            'Department': self.department,
+            'Destination': self.destination,
+            'Basic Salary': self.basic_salary,
+            'Allowance': self.allowance,
+            'Deductions': self.deductions
+        }

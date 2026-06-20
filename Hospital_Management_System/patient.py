da6b0ef53083238b88ba138c02e7e1c1ae239721
Muhammed Rishan T <rishan000rishu@gmail.com>
2026-06-20 15:03:27 +0530

    Created New Function In The Project

--- Hospital_Management_System/patient.py
+++ Hospital_Management_System/patient.py
@@ -24,3 +24,40 @@ def create_patient_file():
             writer = csv.writer(file)
             writer.writerow(HEADER)
 
+def patient_exists(patient_id):
+
+    with open(PATIENT_FILE, 'r') as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+
+            if row['patient_id'] == patient_id:
+                return True
+            
+    return False
+
+def add_patient():
+    patient_id  = input('Enter Patient_ID: ')
+
+    if patient_exists(patient_id):
+        print('Patient Already Exists !')
+        return
+    
+    name = input('Enter Patient Name: ')
+    age = int(input('Enter Patient Age: '))
+    gender  = input('Enter Gender (male/female/others): ')
+    phone = int(input('Enter Phone No: '))
+
+    with open(PATIENT_FILE, 'a', newline='') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(HEADER)
+        writer.writerow([
+            patient_id,
+            name,
+            age,
+            gender,
+            phone
+        ])
+
+    print('Patient Added Successfully.')

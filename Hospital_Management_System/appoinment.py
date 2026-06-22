b4d1a516682380752be5aea2dccfa337836a6a54
Muhammed Rishan T <rishan000rishu@gmail.com>
2026-06-22 16:45:12 +0530

    Added New Function To This Module

--- Hospital_Management_System/appoinment.py
+++ Hospital_Management_System/appoinment.py
@@ -59,3 +59,37 @@ def doctor_exists(doctor_id):
                 return True
             
     return False
+
+def book_appoinment():
+
+    appoinment_id = input('Enter Appoinment_ID: ')
+
+    if appoinment_exists(appoinment_id):
+        print('Appoinment_ID Already Exists !')
+        return
+    
+    patient_id = input('Enter Patient_ID: ')
+
+    if not patient_exists(patient_id):
+        print('Patient_ID Not Found !')
+        return
+    
+    doctor_id = input('Enter Doctor_ID: ')
+
+    if not doctor_exists(doctor_id):
+        print('Doctor_ID Not Found !')
+    
+    appoinment_date = input('Enter Appoinment Date (dd-mm-yyyy): ')
+
+    with open(APPOINMENT_FILE, 'a', newline='') as file:
+        writer = csv.writer(file)
+
+        writer.writerow([
+            appoinment_id,
+            patient_id,
+            doctor_id,
+            appoinment_date
+        ])
+
+    print('\nAppoinment Booked Successfully.')
+    

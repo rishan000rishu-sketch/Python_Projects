1923ae2e3bf247339974987d49871d966219d1c3
Muhammed Rishan T <rishan000rishu@gmail.com>
2026-06-23 19:45:13 +0530

    Created Add Question Function

--- Online_Exam_System/exam.py
+++ Online_Exam_System/exam.py
@@ -21,3 +21,43 @@ def create_exams():
         ])
 
     print('Exam Created Successfully.')
+    
+    add_questions(exam_id)
+
+def add_questions(exam_id):
+
+    count = int(input('Enter No Of Questions: '))
+
+    with open(QUESTION_FILE, 'a', newline='') as file:
+        writer = csv.writer(file)
+
+        for i in range(count):
+
+            print(f'\nQuestion {i+1}')
+
+            question_id = 'Q' + str(uuid.uuid4())[:5]
+
+            question = input("Question: ")
+
+            option1 = input("Option 1: ")
+            option2 = input("Option 2: ")
+            option3 = input("Option 3: ")
+            option4 = input("Option 4: ")
+
+            answer = input(
+                "Correct Answer (exact option text): "
+            )
+
+            writer.writerow([
+                question_id,
+                exam_id,
+                question,
+                option1,
+                option2,
+                option3,
+                option4,
+                answer
+            ])
+
+    print('\nQuestion Added Successfully')
+    

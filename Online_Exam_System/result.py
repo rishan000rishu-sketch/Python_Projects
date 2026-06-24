00b0a508bed86634ce76be6ebd9e9f7d156b7d47
Muhammed Rishan T <rishan000rishu@gmail.com>
2026-06-24 12:31:10 +0530

    Completed The module result With The View Result Function

--- Online_Exam_System/result.py
+++ Online_Exam_System/result.py
@@ -22,4 +22,26 @@ def save_result(user_id,exam_id,score,total):
                 total
             ]
         )
-        
+
+def view_result():
+
+    try:
+        with open(RESULT_FILE, 'r') as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+
+                print('\nRESULTS\n')
+
+                print(f'Result ID: {row['result_id']}')
+                print(f'Student: {row['user_id']}')
+                print(f'Exam ID: {row['exam_id']}')
+                print(f'Score: '
+                      f'{row['score']}/'
+                      f'{row['total']}'
+                )
+                print('-' * 30)
+
+    except FileNotFoundError:
+        print('Results Not Found !')
+    
